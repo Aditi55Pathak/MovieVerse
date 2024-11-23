8bb80e3d27f3f293836f665446387342797370cb
Aditi Pathak <80877301+Aditi55Pathak@users.noreply.github.com>
2024-11-23 15:52:59 +0530

    Update MoviesApplicationTests.java

--- MoviesVerse-API-Backend/src/test/java/movies/api/movies/MoviesApplicationTests.java
+++ MoviesVerse-API-Backend/src/test/java/movies/api/movies/MoviesApplicationTests.java
@@ -4,10 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MoviesApplicationTests {
-
+public class MoviesApplicationTests {
     @Test
     void contextLoads() {
         // This test will pass if the Spring context loads successfully.
     }
 }
+
+
+
